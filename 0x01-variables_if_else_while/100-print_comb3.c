f8af86dc8e8098f014bc23640f5e4acec7df733c
Bennymars <greatbenio07@gmail.com>
2022-09-10 07:37:13 -0700

    New corrections added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-	int i, q;
+	int i;
+	int q;
 
-	for (i =10; i <=19; i++)
+	for (i =10; i <= 19; i++)
 	{
-		for (q =10; <=19; q++)
+		for (q =10; <= 19; q++)
 		{
 			if ((q % 10) > (i % 10))
 			{
 				putchar((i % 10) + '0');
 				putcahr((q % 10) + '0');
-				if (i !=18 || j !=19)
+				if (i != 18 || j != 19)
 				{
 					putchar(',');
 					putchar(' ');
