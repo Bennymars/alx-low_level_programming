00fedd3ca97741e2ff3258ca155b7e3d6245fd7c
Bennymars <greatbenio07@gmail.com>
2022-09-10 08:30:12 -0700

    Added code script

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int b;
+	int m;
+
+	for (b = 0; b <= 99; b++)
+	{
+		for (m = b + 1; m <= 99; m++)
+		{
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			putchar(' ');
+			putchar(m / 10 + '0');
+			putchar(m % 10 + '0');
+
+			if (!(b == 98 && m == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
