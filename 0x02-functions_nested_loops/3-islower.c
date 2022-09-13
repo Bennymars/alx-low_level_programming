e7d4f0f3c1e4bc740cd1e8c6a476a815edec2d25
Bennymars <greatbenio07@gmail.com>
2022-09-13 09:54:20 -0700

    Added changes

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ *
+ * Return: void
+ */
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
