fda237a1bb020f2fbab0be6461c5857e73378210
Bennymars <greatbenio07@gmail.com>
2022-09-13 09:18:39 -0700

    New changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,12 @@
  */
 void print_alphabet(void)
 {
-	char i;
+	char letter = 'a';
 
-	for (i = 'a'; i <= 'z';)
+	while (letter <= 'z')
 	{
-		_putchar(i);
-		i++;
+		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
 }
