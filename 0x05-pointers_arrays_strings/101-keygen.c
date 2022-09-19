45d74873a761aa4f05fca5623d5c86b61547bafa
Bennymars <greatbenio07@gmail.com>
2022-09-19 09:32:57 -0700

    Added changes to script

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,17 +8,19 @@
  */
 int main(void)
 {
-	int sum;
-	char c;
+	int i = 0, j = 0;
+	time_t t;
 
-	srand(time(NULL));
-	while (sum <= 2645)
+	srand((unsigned int) time(&t));
+	while (j < 2772)
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		i = rand() % 128;
+		if ((j + i) > 2772)
+			break;
+		j = j + i;
+		printf("%c", i);
 	}
-	putchar(2772 - sum);
-
+	printf("%c\n", (2772 - j));
 	return (0);
 }
+
