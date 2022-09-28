423f4c514a184658dba824e99c3c8e81845e39f2
Bennymars <greatbenio07@gmail.com>
2022-09-28 06:48:06 -0700

    Added new changes

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,13 +2,13 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * 
  * @s: pointer to string params
+ *
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
