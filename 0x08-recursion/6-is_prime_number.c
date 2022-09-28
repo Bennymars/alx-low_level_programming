21a1c752603c70539b67c879ed342bd172eb065e
Bennymars <greatbenio07@gmail.com>
2022-09-28 07:51:23 -0700

    Added new script

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * divisors - number is prime
+ * @n: integer paramtr 1
+ * @m: integer paramtr 2
+ */
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - checks if a given number is prime
+ * @n: integer paramtr
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
